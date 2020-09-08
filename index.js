f41f030a270eb042fef2f1d5389b7895635ef841
bastar2 <adriiiel.alvarez@gmail.com>
2020-09-08 20:24:40 +0000

    add cors

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ const routesApp = require(__dirname+'/routes/routesApp.js');
 const noSniff = require('dont-sniff-mimetype');
 const path = require('path');
 
+
 //SETTINGS
 app.set('port', process.env.PORT || 5000);
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+}
 
 app.use('/js', express.static(path.join(__dirname, '/js')));
 app.use('/css', express.static(path.join(__dirname, '/css')));
 app.use('/img', express.static(path.join(__dirname, '/img')));
 app.use('/lista', express.static(path.join(__dirname, '/listaBebidas.json')));
 
+app.use(allowCrossDomain);
 app.use(noSniff());
 app.use(routesApp);
 
